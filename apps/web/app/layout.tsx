df6067490a0e302b7517efdaa9c830cfc46dd3ce
smit2402 <smitsavani2402@gmail.com>
2024-07-09 00:54:40 +0530

    added github action

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html >
+    <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body> 
