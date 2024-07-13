a6ba179df8e58232553f902f7302ebf241205cfa
smit2402 <smitsavani2402@gmail.com>
2024-07-13 17:16:04 +0530

    added github action

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/window.svg"
+            src="/docs/window.svg"
             alt="Window icon"
             width={16}
             height={16}
@@ -93,7 +93,7 @@ export default function Home() {
         >
           <Image
             aria-hidden
-            src="/globe.svg"
+            src="/docs/globe.svg"
             alt="Globe icon"
             width={16}
             height={16}
